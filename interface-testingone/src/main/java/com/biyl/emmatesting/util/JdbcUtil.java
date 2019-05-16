0df76f60d695aac99e15f6678c28eca235e063c6
andrew-303 <303247051@qq.com>
2019-05-16 21:45:00 +0800

    完善JdbcUtil

--- interface-testingone/src/main/java/com/biyl/emmatesting/util/JdbcUtil.java
+++ interface-testingone/src/main/java/com/biyl/emmatesting/util/JdbcUtil.java
@@ -43,5 +43,152 @@ public class JdbcUtil {
      * @param sql
      * @return
      */
+	public static int executeNonQuery(String sql) {
+        int result = 0;
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = getConnection();
+            stmt = conn.createStatement();
+            result = stmt.executeUpdate(sql);
+        } catch (SQLException err) {
+            err.printStackTrace();
+            free(null, stmt, conn);
+        } finally {
+            free(null, stmt, conn);
+        }
+        return result;
+    }
+
+    /**
+     * 增删改【Add、Delete、Update】
+     *
+     * @param sql
+     * @param obj
+     * @return
+     */
+    public static int executeNonQuery(String sql, Object... obj) {
+        int result = 0;
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            for (int i = 0; i < obj.length; i++) {
+                pstmt.setObject(i + 1, obj[i]);
+            }
+            result = pstmt.executeUpdate();
+        } catch (SQLException err) {
+            err.printStackTrace();
+            free(null, pstmt, conn);
+        } finally {
+            free(null, pstmt, conn);
+        }
+        return result;
+    }
+
+    /**
+     * 查【Query】
+     *
+     * @param sql
+     * @return
+     */
+    public static ResultSet executeQuery(String sql) {
+        Connection conn = null;
+        Statement stmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+        } catch (SQLException err) {
+            err.printStackTrace();
+            free(rs, stmt, conn);
+        }
+        return rs;
+    }
+
+    /**
+     * 查【Query】
+     *
+     * @param sql
+     * @param obj
+     * @return
+     */
+    public static ResultSet executeQuery(String sql, Object... obj) {
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            for (int i = 0; i < obj.length; i++) {
+                pstmt.setObject(i + 1, obj[i]);
+            }
+            rs = pstmt.executeQuery();
+        } catch (SQLException err) {
+            err.printStackTrace();
+            free(rs, pstmt, conn);
+        }
+        return rs;
+    }
+    
+    /**
+     * 释放【ResultSet】资源
+     *
+     * @param rs
+     */
+    public static void free(ResultSet rs) {
+        try {
+            if (rs != null) {
+                rs.close();
+            }
+        } catch (SQLException err) {
+            err.printStackTrace();
+        }
+    }
+
+    /**
+     * 释放【Statement】资源
+     *
+     * @param st
+     */
+    public static void free(Statement st) {
+        try {
+            if (st != null) {
+                st.close();
+            }
+        } catch (SQLException err) {
+            err.printStackTrace();
+        }
+    }
+
+    /**
+     * 释放【Connection】资源
+     *
+     * @param conn
+     */
+    public static void free(Connection conn) {
+        try {
+            if (conn != null) {
+                conn.close();
+            }
+        } catch (SQLException err) {
+            err.printStackTrace();
+        }
+    }
+
+    /**
+     * 释放所有数据资源
+     *
+     * @param rs
+     * @param st
+     * @param conn
+     */
+    public static void free(ResultSet rs, Statement st, Connection conn) {
+        free(rs);
+        free(st);
+        free(conn);
+    }
 	
 }
