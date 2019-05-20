084757077de98e6c5f87c27f4340fa2d97947960
andrew-303 <303247051@qq.com>
2019-05-20 22:31:21 +0800

    完善测试case的工厂类--生成测试类20190520

---
+++ interface-testingone/src/main/java/com/biyl/emmatesting/testcase/user/test_001_test.java
@@ -0,0 +1,31 @@
+package com.biyl.emmatesting.testcase.user; 
+
+import java.util.Map;
+import org.apache.log4j.Logger;
+import org.testng.annotations.Test;
+import com.biyl.emmatesting.util.JdbcUtil;
+import com.biyl.emmatesting.util.UnirestUtil;
+import com.biyl.emmatesting.base.BaseParpare;
+
+public class test_001_test extends BaseParpare { 
+ static Logger logger = Logger.getLogger(test_001_test.class.getName());
+	@Test(dataProvider = "testData") 
+	public void testCase(Map<String, String> data) { 
+		String pre_condition = data.get("pre_condition"); 
+		/******************************************************************************** 
+		 *********************** 用例中的pre_condition列不为空时，执行相应的SQL语句******************** 
+		 ********************************************************************************/ 
+		if (pre_condition.length() > 0) { 
+		logger.info("**********************************正在执行数据准备操作*********************************"); 
+			String[] Array = pre_condition.split(";\n"); 
+			for (int i = 0; i < Array.length; i++) { 
+				JdbcUtil.executeNonQuery(Array[i]); 
+			} 
+		logger.info("**********************************执行数据准备操作完成*********************************"); 
+		} 
+		/******************************************************************************** 
+		 *********************************** 进行接口测试，并验证测试结果*************************** 
+		 ********************************************************************************/ 
+		UnirestUtil.InterfaceTest(data);  
+	}
+}
